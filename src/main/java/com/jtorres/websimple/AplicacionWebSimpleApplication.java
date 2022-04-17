fb0f1048fb35ca58381c662a4104394d4a4610a2
Joaquin Torres <mod4rchive@gmail.com>
2022-04-17 02:14:47 -0400

    inicial

---
+++ src/main/java/com/jtorres/websimple/AplicacionWebSimpleApplication.java
@@ -0,0 +1,21 @@
+package com.jtorres.websimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AplicacionWebSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionWebSimpleApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String entrance() {
+		return String.format("Aplicación web muy simple");
+	}
+
+}
---
+++ src/test/java/com/jtorres/websimple/AplicacionWebSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtorres.websimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
